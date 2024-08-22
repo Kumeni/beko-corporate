a2c07a3b71fb3ac2156efd6e3d1f3118a8d6798b
Kumeni <samsonkumeni8@gmail.com>
2024-08-22 11:21:01 +0300

    landing section images

---
+++ scripts/swiper/initialize-swiper.js
@@ -0,0 +1,33 @@
+let swiperLanding = new Swiper('.homepage-landing', {
+  // Optional parameters
+  speed:6000,
+  spaceBetween:0,
+  slidesPerView:1,
+  direction: 'horizontal',
+  loop: true,
+  effect:'coverflow',
+  //effect:'cards',
+  
+  // If we need pagination
+  allowTouchMove:true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable:true,
+  },
+
+  autoplay:{
+    delay:100,
+    disableOnInteraction:false,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
